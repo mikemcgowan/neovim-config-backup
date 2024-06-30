a66deb494ef5c2146781c8f2b6698a1f8bc890a7
Mike McGowan <mike.mcgowan.75@gmail.com>
2024-06-30 20:42:25 +0100

    Autocmd for Kotlin

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,9 @@
 require "nvchad.options"
 
--- add yours here!
-
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- added by Mike
-o.tabstop = 4
-o.shiftwidth = 4
 o.expandtab = true
 o.list = true
 o.listchars = "tab:>-,space:·"
---
+++ lua/plugins/autocmds.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = false,
+    config = function()
+      local autocmd = vim.api.nvim_create_autocmd
+      autocmd("FileType", {
+        pattern = "kotlin",
+        callback = function()
+          vim.bo.tabstop = 4
+          vim.bo.shiftwidth = 4
+        end
+      })
+    end
+  }
+}
