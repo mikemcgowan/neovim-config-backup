3745899d4dea830a61762eb6fa2266399434735a
Mike McGowan <mike.mcgowan@getbridge.com>
2024-07-17 17:50:13 +0100

    Remove unnecessary key mapping (revert mappings.lua to out-of-the-box)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- added by Mike
-map("n", "<Leader>/", ":nohlsearch<CR>", { noremap = true, silent = true })
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
